b0f316c9c30d90f2a96266ed85bc72faee10d354
Velina Valcheva <velina.valcheva@gmail.com>
2024-10-02 10:24:39 +0100

    feat: initial test

--- __tests__/warehouse.test.js
+++ __tests__/warehouse.test.js
@@ -1,5 +1,14 @@
-describe("Buy CDs", () => {
+class Record {
+  quantity;
+
+  constructor() {
+    this.quantity = 5;
+  }
+}
+
+describe("Buy CDs:", () => {
   it("when payment fails, inventory remains the same", () => {
-    expect(cd.getQuantity()).toBe(5);
+    const record = new Record();
+    expect(record.quantity).toBe(5);
   });
 });
